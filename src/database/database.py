c3a6207f48fda5290386dc11491f0b009867a9a5
przemek2709 <przemek2709@gmail.com>
2015-05-25 23:42:51 +0200

    Update Database files

--- src/database/database.py
+++ src/database/database.py
@@ -2,11 +2,6 @@ __author__ = 'Przemek'
 
 import sqlite3
 import abc
-import logging
-import sys
-import traceback
-
-from PyQt4 import QtGui
 
 
 class Database(object):
@@ -16,12 +11,9 @@ class Database(object):
     def connect(db_name):
         try:
             sqlite3.connect(db_name)
-            logging.log(logging.DEBUG, "Connected to the {} database".format(db_name))
         except sqlite3.DatabaseError:
             print "Error. Can't connect to the database."
-            ex_type, ex_value, ex_traceback = sys.exc_info()
-            lines = traceback.format_exception(ex_type, ex_value, ex_traceback)
-            message = ''.join("!! " + line for line in lines)
+
 
     @staticmethod
     def createTable(table_name, sql):
@@ -31,17 +23,9 @@ class Database(object):
             result = cursor.fetchall()
             keep_table = True
             if len(result) == 1:
-                messBox = QtGui.QMessageBox()
-                response = QtGui.QMessageBox.question(messBox, "Warning!", "The table {0} already exists,"
-                                                                           "recreate it?".format(table_name),
-                                                      QtGui.QMessageBox.Ok, QtGui.QMessageBox.No)
-                if response == 1024:
-                    print "recreating..."
-                    keep_table = False
-                    cursor.execute("drop table if exists {0}".format(table_name))
-                    db.commit()
-                else:
-                    print "table kept"
+                keep_table = False
+                cursor.execute("drop table if exists {0}".format(table_name))
+                db.commit()
             else:
                 keep_table = False
             if not keep_table:
@@ -49,11 +33,11 @@ class Database(object):
                 db.commit()
 
     @staticmethod
-        def dropTable(db_name, tableName):
-            with sqlite3.connect(db_name) as db:
-                cursor = db.cursor()
-                cursor.execute("drop table {0}".format(tableName))
-                db.commit()
+    def dropTable(db_name, tableName):
+        with sqlite3.connect(db_name) as db:
+            cursor = db.cursor()
+            cursor.execute("drop table {0}".format(tableName))
+            db.commit()
 
     @staticmethod
     @abc.abstractmethod
@@ -61,16 +45,6 @@ class Database(object):
         with sqlite3.connect(Database.DB_NAME) as db:
             cursor = db.cursor()
             cursor.execute(sql, values)
-            db.commit()
-
-    @staticmethod
-    @abc.abstractmethod
-    def update(sql, data):
-        with sqlite3.connect(Database.DB_NAME) as db:
-            cursor = db.cursor()
-            if len(data) == 1:
-                data = (data,)
-            cursor.execute(sql, data)
             db.commit()
 
     @staticmethod
--- src/database/tasksDatabase.py
+++ src/database/tasksDatabase.py
@@ -1,1 +1,52 @@
 __author__ = 'Przemek'
+
+import sqlite3
+
+from database import Database
+
+class TasksDatabase(Database):
+
+    tableName = "Tasks"
+    sql = """CREATE TABLE "Tasks"(
+                topic TEXT PRIMARY KEY NOT NULL,
+                priority INTEGER NOT NULL,
+                profit INTEGER NOT NULL,
+                execution_time INTEGER NOT NULL,
+                previous_task_1 INTEGER DEFAULT NULL,
+                previous_task_2 INTEGER DEFAULT NULL);
+                """
+
+    @staticmethod
+    def insert(topic, **kwargs):
+        # dla Adama :)
+        #sql = "insert into Tasks ()"
+        #Database.insert()
+        pass
+
+    @staticmethod
+    def delete(data):
+        assert type(data) == str
+        sql = "DELETE FROM Tasks WHERE topic=?"
+        Database.delete(sql, data)
+
+    @staticmethod
+    def selectAll():
+        sql = "SELECT * FROM Tasks"
+        return Database.selectAll(sql)
+
+    @staticmethod
+    def selectOne(topic):
+        assert type(topic) == str
+        sql = "SELECT * FROM Tasks WHERE topic={}".format(topic)
+        return Database.selectOne(sql)
+
+
+
+
+def main():
+    db = TasksDatabase
+    res = db.createTable('Tasks', db.sql)
+    print res
+
+if __name__ == "__main__":
+    main()
