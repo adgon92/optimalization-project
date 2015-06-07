Correct task real cost calculation

src/solvers/alg_solver.py
@@ -2,7 +2,9 @@ __author__ = 'gontarz'
 
 import math
 import copy
+import numpy as np
 from settings import START_VECTOR
+from database.database import TopicDatabase
 
 
 class Objective(object):
@@ -24,12 +26,15 @@ class Objective(object):
         return max(profits)
 
     def _check_order(self, tasks):
+        print(tasks)
         for task in tasks[1:]:
-            before = tasks[:tasks.index(tasks)]
+            before = tasks[:tasks.index(task)]
             before_ids = [prev_task.id for prev_task in before]
             if task.prev_task_1 is not None:
                 if not task.prev_task_1 in before_ids:
+                    print('Punishing {}. Cost before: {}'.format(task.topic, task.cost))
                     task.punish()
+                    print('     Cost after: {}'.format(task.cost))
                 if task.prev_task_2 is not None:
                     if not task.prev_task_2 in before_ids:
                         task.punish()
@@ -50,16 +55,22 @@ class Solver(object):
     NUMBER_OF_ACCEPTED_SOLUTIONS = 0.0
     START_WORSE_SOL_ACCEPTANCE = 0.7
     END_WORSE_SOL_ACCEPTANCE = 0.001
-    INITIAL_TEMPERATURE = _to_log(START_WORSE_SOL_ACCEPTANCE)
-    FINAL_TEMPERATURE = _to_log(END_WORSE_SOL_ACCEPTANCE)
-    REDUCTION_PER_CYCLE = _get_reduction_per_cycle(INITIAL_TEMPERATURE, FINAL_TEMPERATURE, NUMBER_OF_CYCLES)
 
     def __init__(self, method):
-        self._cooling_method = self.cooling_method(method)
+        self.initial_temperature = self._to_log(self.START_WORSE_SOL_ACCEPTANCE)
+        self.final_temperature = self._to_log(self.END_WORSE_SOL_ACCEPTANCE)
+        self.reduction_per_cycle = self._get_reduction_per_cycle(
+            self.initial_temperature,
+            self.final_temperature,
+            self.NUMBER_OF_CYCLES
+        )
+        self.cooling_method = method
+        self.start_tasks = self._get_tasks(START_VECTOR)
+        self.objective = Objective(self.start_tasks)
 
     @property
     def cooling_method(self):
-        factor = self.FINAL_TEMPERATURE/self.INITIAL_TEMPERATURE
+        factor = self.final_temperature/self.initial_temperature
         boltzmans_const = 1.0/(self.NUMBER_OF_CYCLES-1.0)
         cooling = {
             'linear': 1/boltzmans_const,
@@ -75,8 +86,14 @@ class Solver(object):
             raise AttributeError('Allowed vales are: {}'.format(allowed_values))
         self._cooling_method = method
 
-
     def solve(self):
         ini_vector = START_VECTOR
+        tasks = self._get_tasks(ini_vector)
+        best = self.objective.get(tasks)
+        solutions = np.zeros(self.NUMBER_OF_CYCLES + 1)
+        solutions[0] = best
+        temperature = self.initial_temperature
 
-
+    def _get_tasks(self, ids):
+        with TopicDatabase() as db:
+            return [db.select_one('Temat{}'.format(task_id)) for task_id in ids]