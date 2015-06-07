96d7f27b80874dd39d9c6d62b2d5743364a590be
przemek2709 <przemek2709@gmail.com>
2015-06-07 23:39:06 +0200

    New GIU

--- src/gui/mainWindow.py
+++ src/gui/mainWindow.py
@@ -1,63 +1,59 @@
 __author__ = 'Przemek'
 
-import Tkinter
+from Tkinter import *
+#from Tkinter import ttk
+import ttk
+
+class mainWindow():
+
+    root = Tk()
+    root.title("Simulated annealing")
+
+    mainframe = ttk.Frame(root, padding="20 20 20 20", width=200, height=100)
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    mainframe.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
+
+    entryframe = ttk.Frame(root, padding="20 20 20 20", width=200, height=100)
+    entryframe.grid(column=0, row=3, sticky=(N, W, E, S))
+    entryframe.columnconfigure(0, weight=1)
+    entryframe.rowconfigure(0, weight=1)
+
+    buttonsframe = ttk.Frame(root, padding="20 20 20 20", width=200, height=100)
+    buttonsframe.grid(column=0, row=6, sticky=(N, W, E, S))
+    buttonsframe.columnconfigure(0, weight=1)
+    buttonsframe.rowconfigure(0, weight=1)
+
+
+    entry1 = StringVar()
+    entry2 = StringVar()
+    entry3 = StringVar()
+
+    entry1 = ttk.Entry(entryframe, width=7, textvariable=entry1)
+    entry1.grid(column=1, row=0, sticky=(W, E))
+
+    entry2 = ttk.Entry(entryframe, width=7, textvariable=entry2)
+    entry2.grid(column=1, row=1, sticky=(W, E))
+
+    entry3 = ttk.Entry(entryframe, width=7, textvariable=entry3)
+    entry3.grid(column=1, row=2, sticky=(W, E))
+
+    ttk.Label(mainframe, text="TASK OPTIMIZATION").grid(column=1, row=1, sticky=N, columnspan=3)
+    ttk.Label(entryframe, text="parameter 1").grid(column=0, row=0, sticky=E)
+    ttk.Label(entryframe, text="parameter 2").grid(column=0, row=1, sticky=E)
+    ttk.Label(entryframe, text="parameter 3").grid(column=0, row=2, sticky=E)
+
+    ttk.Button(buttonsframe, text="Start").grid(column=1, row=5, sticky=W)
+    ttk.Button(buttonsframe, text="Quit", command=root.quit).grid(column=3, row=5, sticky=W)
+
+
+
+    for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
 
 
 
-class mainWindow(Tkinter.Tk):
-    def __init__(self,parent):
-        Tkinter.Tk.__init__(self,parent)
-        self.parent = parent
-        self.initialize()
-
-    def initialize(self):
-        self.grid()
-
-        self.entryVariable = Tkinter.StringVar()
-        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
-        self.entry.grid(column=0,row=0,sticky='EW')
-        self.entry.bind("<Return>", self.OnPressEnter)
-        self.entryVariable.set(u"Enter text here.")
-
-        button = Tkinter.Button(self,text=u"Click me !",
-                                command=self.OnButtonClick)
-        button.grid(column=1,row=0)
-
-        self.labelVariable = Tkinter.StringVar()
-        label = Tkinter.Label(self,textvariable=self.labelVariable,
-                              anchor="w",fg="white",bg="blue")
-        label.grid(column=0,row=1,columnspan=2,sticky='EW')
-        self.labelVariable.set(u"Hello !")
-
-        self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
-        self.update()
-        self.geometry(self.geometry())
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
-
-    def OnButtonClick(self):
-        self.labelVariable.set( self.entryVariable.get()+" (You clicked the button)" )
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
-
-    def OnPressEnter(self,event):
-        self.labelVariable.set( self.entryVariable.get()+" (You pressed ENTER)" )
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
+    root.mainloop()
 
 
 
-if __name__ == "__main__":
-    app = mainWindow(None)
-    app.title('Simulated annealing application')
-    app.mainloop()
-
-#def main():
-#    top = Tkinter.Tk()
-#    # Code to add widgets will go here...
-#    top.mainloop()
-
-
-#main()
