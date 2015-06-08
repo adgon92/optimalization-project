743b9d0725d89619f9cf9a9e4e588133de1341fb
przemek2709 <przemek2709@gmail.com>
2015-06-08 22:09:32 +0200

    Better plots

--- src/plot/ploter.py
+++ src/plot/ploter.py
@@ -8,8 +8,23 @@ class Ploter:
     def __init__(self):
         pass
 
-    def plot(self, objectives):
+    def plot_temperature(self, temperature, cooling_method, initial_temperature, numb_of_cycles):
+        plt.plot(temperature, 'b.-')
+        plt.xlabel('Number of cycles')
+        plt.ylabel('Temperature')
+        plt.text(0.7*numb_of_cycles, max(temperature),
+                 'Cooling method: '.join(cooling_method)
+                 +'\nInitial temperature: '.join(initial_temperature)
+                 +'\nNumb of cycles: '.join(numb_of_cycles))
+
+    def plot(self, objectives, cooling_method, initial_temperature, numb_of_cycles):
         plt.plot(objectives,'r.-')
+        plt.xlabel('Number of cycles')
+        plt.ylabel('Quality of solution')
+        plt.text(0.7*numb_of_cycles, max(objectives),
+                 'Cooling method: '.join(cooling_method)
+                 +'\nInitial temperature: '.join(initial_temperature)
+                 +'\nNumb of cycles: '.join(numb_of_cycles))
 
     def save(self, path):
         plt.savefig(path)
